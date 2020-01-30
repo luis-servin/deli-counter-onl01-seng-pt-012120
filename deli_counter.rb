371e8baa8b821c03cb43f54335e7e033cd8cde35
Luis Alberto Servin <lservin2015@gmail.com>
2020-01-30 03:10:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,9 @@ def line (deli)
   if deli.length == 0 
     puts "The line is currently empty."
   else
+    str = "The line is currently:"
     deli.each.with_index{1} do |person, 1|
-      "#{1}. #{person}"
+      str "#{1}. #{person}"
   end
 end
 
