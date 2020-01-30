f472dec0b94a6d34c9923cc412ea58e8a52948c6
Luis Alberto Servin <lservin2015@gmail.com>
2020-01-30 03:23:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(deli)
     puts "The line is currently empty."
   else
     str = "The line is currently:"
-    deli.each.with_index(1) do |person, 1|
+    deli.each.with_index(1) do |person, i|
       str += " #{i}. #{person}"
     end
     puts str
